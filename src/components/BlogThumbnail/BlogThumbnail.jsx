939fff96196519480e99cbf2ab3d34d0ba93b304
Steven Barash <stevenbarash6@gmail.com>
2020-01-30 23:16:04 -0500

    fix typo

--- src/components/BlogThumbnail/BlogThumbnail.jsx
+++ src/components/BlogThumbnail/BlogThumbnail.jsx
@@ -4,7 +4,7 @@ export default function BlogThumbnail(props) {
   return (
     //displays the title of the post, a preview of the first 100 characters of the post as well as post author
     <div>
-      {/* clicking on the post appends the id of the post to a query in the URL to beused by the "post" component to display the proper post */}
+      {/* clicking on the post appends the id of the post to a query in the URL to be used by the "BlogPost" component to display the proper post */}
       <a href={"/post?id=" + props.id}>
         <h2 className="title">{props.title}</h2>
         <p className="body-preview">{props.body.slice(0, 100) + "..."}</p>
